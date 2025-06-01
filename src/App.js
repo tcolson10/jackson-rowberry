ed485e08b7d8536a0693f045627ca1ad924cca7a
tcolson10 <trevor@colson.dev>
2025-06-01 15:25:06 -0600

    bye bye mouse

--- src/App.js
+++ src/App.js
@@ -507,13 +507,6 @@ const App = () => {
 						</div>
 					</div>
 				</div>
-
-				{/* Scroll indicator */}
-				<div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-					<div className="w-6 h-10 border-2 border-slate-400 rounded-full flex justify-center">
-						<div className="w-1 h-3 bg-slate-400 rounded-full mt-2 animate-pulse"></div>
-					</div>
-				</div>
 			</section>
 
 			{/* Portfolio Section */}
